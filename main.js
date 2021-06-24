f29d36091641c1d41408a36245729c2a0395a23b
Irina-88 <schastie-88@bk.ru>
2021-06-24 23:01:33 +0200

    Create: add new enemy, music, score, crash

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+
+
+const MAX_ENEMY = 9;
+
 const score = document.querySelector('.score'),
   start = document.querySelector('.start'),
   gameArea = document.querySelector('.gameArea');
   car = document.createElement('div');
+
+const music = new Audio('audio.mp3');
+
 car.classList.add('car');
 
 start.addEventListener('click', startGame);
@@ -19,16 +26,19 @@ const setting = {
   start: false,
   score: 0,
   speed: 3,
-  traffic: 3
-}
+  traffic: 2.5
+};
 
 function getQuantityElements(heightElement) {
   return document.documentElement.clientHeight / heightElement + 1;
 }
 
-function startGame() {
-  start.classList.add('hide');
+const getRandomEnemy = (max) => Math.floor((Math.random() * max) + 1);
 
+function startGame() {
+  music.play();
+  start.classList.add('hide');
+  gameArea.innerHTML = '';
   for (let i = 0; i < getQuantityElements(100); i++) {
     const line = document.createElement('div');
     line.classList.add('line');
@@ -43,25 +53,36 @@ function startGame() {
     enemy.y = -100 * setting.traffic * (i + 1);
     enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
     enemy.style.top = enemy.y + 'px';
-    enemy.style.background = 'transparent url(./image/bugatti.png) center / cover no-repeat'
+    enemy.style.background = `
+    transparent 
+    url(./image/enemy${getRandomEnemy(MAX_ENEMY)}.png) 
+    center / cover 
+    no-repeat`;
     gameArea.appendChild(enemy);
   }
 
+  setting.score = 0;
   setting.start = true;
   gameArea.appendChild(car);
+  car.style.left = '125px';
+  car.style.top = 'auto';
+  car.style.bottom = '10px';
   setting.x = car.offsetLeft;
   setting.y = car.offsetTop;
   requestAnimationFrame(playGame);
-};
+}
 
-function playGame() {
-  
+function playGame(){
+
   if (setting.start) {
+    setting.score += setting.speed;
+    score.innerHTML = 'SCORE<br>' + setting.score;
     moveRoad();
     moveEnemy();
     if (keys.ArrowLeft && setting.x > 0) {
       setting.x -= setting.speed;
     }
+
     if (keys.ArrowRight && setting.x < (gameArea.offsetWidth - car.offsetWidth)) {
       setting.x += setting.speed;
     }
@@ -81,37 +102,54 @@ function playGame() {
   } 
 }
 
-function startRun(event){
-  event.preventDefault();
-  keys[event.key] = true;
-};
+function startRun(event) {
+  if (keys.hasOwnProperty(event.key)) {
+      event.preventDefault();
+      keys[event.key] = true;
+  }  
+}
 
-function stopRun(event){
-  event.preventDefault();
-  keys[event.key] = false;
-};
+function stopRun(event) {
+  if (keys.hasOwnProperty(event.key)) {
+      event.preventDefault();
+      keys[event.key] = false;
+  }
+}
 
-function moveRoad(){
+function moveRoad() {
   let lines = document.querySelectorAll('.line');
-  lines.forEach(function(line){
+  lines.forEach(function(line) {
     line.y += setting.speed;
     line.style.top = line.y + 'px';
-
     if (line.y >=document.documentElement.clientHeight){
       line.y = -100;
     }
-  })
+  });
 }
 
-function moveEnemy(){
-   let enemy = document.querySelectorAll('.enemy');
+function moveEnemy() {
+  let enemy = document.querySelectorAll('.enemy');
+  
   enemy.forEach(function(item){
+    let carRect = car.getBoundingClientRect();
+    let enemyRect = item.getBoundingClientRect();
+
+    if (carRect.top <= enemyRect.bottom && 
+      carRect.right >= enemyRect.left && 
+      carRect.left <= enemyRect.right && 
+      carRect.bottom >= enemyRect.top) {
+        setting.start = false;
+        console.log('ДТП');
+        start.classList.remove('hide');
+        start.style.top = score.offsetHeight;
+    }
+
     item.y += setting.speed / 2;
     item.style.top = item.y + 'px';
     if (item.y >=document.documentElement.clientHeight){
-    item.y = -100 * setting.traffic;
-    item.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
-  }
+        item.y = -100 * setting.traffic;
+        item.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+    }
   });
 
 }
